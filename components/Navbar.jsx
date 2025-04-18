df5e01a8b933dcadeefd3145ed4dd838988e858f
Marianne002 <gdj.marianne@gmail.com>
2025-04-18 10:27:47 +0200

    👌 IMPROVE: Navbar icons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,5 @@
 // components/Navbar.jsx
 import React from 'react';
-// import { ShoppingCartIcon, MenuIcon, SearchIcon, UserIcon } from 'lucide-react'
 
 const Navbar = () => {
     return (
@@ -8,12 +7,15 @@ const Navbar = () => {
             <header className="bg-white shadow-sm sticky top-0 z-10">
                 <div className="container mx-auto px-4 py-4 flex items-center justify-between">
                     <div className="flex items-center">
-                        <button className="mr-4 md:hidden">
-                           Menu Icon
-                        </button>
                         <a href="/" className="text-xl font-bold text-gray-800">
-                            BoxCraft
+                            Re_Fashion
                         </a>
+                        <button className="ml-4">
+                            {/* Menu Icon */}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+                            </svg>
+                        </button>
                     </div>
                     <nav className="hidden md:flex space-x-6">
                         <a href="/" className="text-gray-800 hover:text-gray-600">
@@ -34,13 +36,24 @@ const Navbar = () => {
                     </nav>
                     <div className="flex items-center space-x-4">
                         <button className="text-gray-700 hover:text-gray-900">
-                            Search Icon
+                            {/* Search Icon */}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                            </svg>
                         </button>
                         <button className="text-gray-700 hover:text-gray-900">
-                            User Icon
+                            {/* User Icon */}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                            </svg>
                         </button>
                         <button className="text-gray-700 hover:text-gray-900 relative">
-                            Cart Icon
+                            {/* Cart Icon */}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-basket2" viewBox="0 0 16 16">
+                                <path d="M4 10a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0zm3 0a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0zm3 0a1 1 0 1 1 2 0v2a1 1 0 0 1-2 0z" />
+                                <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-.623l-1.844 6.456a.75.75 0 0 1-.722.544H3.69a.75.75 0 0 1-.722-.544L1.123 8H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.163 8l1.714 6h8.246l1.714-6z" />
+                            </svg>
                             <span className="absolute -top-2 -right-2 bg-blue-600 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
                                 0
                             </span>
